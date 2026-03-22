05201dceda0ce4743d5f9263aab677f5dc9ef238
Dhenuka Dudde <dhenukadudde@gmail.com>
2026-03-23 00:32:45 +0530

    Update streamlit_app.py

--- AI_Resume_Analyzer/streamlit_app.py
+++ AI_Resume_Analyzer/streamlit_app.py
@@ -25,55 +25,55 @@ job_description = st.text_area(
 
 if st.button("Analyze Resume"):
     if uploaded_file and job_description:
-    st.subheader("Analyzing Resume...")
-
-    resume_text = extract_text_from_pdf(resume_file)
-
-    resume_skills = extract_skills_with_llm(resume_text)
-
-    jd_skills = extract_skills_with_llm(job_description)
-
-
-    # Skill Match Score
-    skill_score, matched, missing = calculate_skill_score(
-        resume_skills,
-        jd_skills
-    )
-
-
-    # Semantic Similarity Score
-    semantic_score = semantic_match_score(
-        resume_text,
-        job_description
-    )
-
-
-    # Final ATS Score (combined)
-    final_score = int((skill_score * 0.6) + (semantic_score * 0.4))
-
-
-    st.subheader("ATS Score")
-
-    st.progress(final_score / 100)
-
-    st.write(f"Score: {final_score}%")
-
-
-    st.subheader("Matched Skills")
-
-    st.write(matched)
-
-
-    st.subheader("Missing Skills")
-
-    st.write(missing)
-
-
-    st.subheader("AI Resume Feedback")
-
-    feedback = get_ai_feedback(resume_text, job_description)
-
-    st.write(feedback)
+        st.subheader("Analyzing Resume...")
+    
+        resume_text = extract_text_from_pdf(resume_file)
+    
+        resume_skills = extract_skills_with_llm(resume_text)
+    
+        jd_skills = extract_skills_with_llm(job_description)
+    
+    
+        # Skill Match Score
+        skill_score, matched, missing = calculate_skill_score(
+            resume_skills,
+            jd_skills
+        )
+    
+    
+        # Semantic Similarity Score
+        semantic_score = semantic_match_score(
+            resume_text,
+            job_description
+        )
+    
+    
+        # Final ATS Score (combined)
+        final_score = int((skill_score * 0.6) + (semantic_score * 0.4))
+    
+    
+        st.subheader("ATS Score")
+    
+        st.progress(final_score / 100)
+    
+        st.write(f"Score: {final_score}%")
+    
+    
+        st.subheader("Matched Skills")
+    
+        st.write(matched)
+    
+    
+        st.subheader("Missing Skills")
+    
+        st.write(missing)
+    
+    
+        st.subheader("AI Resume Feedback")
+    
+        feedback = get_ai_feedback(resume_text, job_description)
+    
+        st.write(feedback)
 
     else:
         st.warning("Please upload resume and enter job description")
