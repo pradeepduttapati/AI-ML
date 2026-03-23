b7f026c1a8413772a6562f3697b1b1ffab1f5c7d
Dhenuka Dudde <dhenukadudde@gmail.com>
2026-03-23 23:30:54 +0530

    Update api.py

--- AI_Resume_Analyzer/api.py
+++ AI_Resume_Analyzer/api.py
@@ -1,52 +1,55 @@
 print("app is starting")
-@app.post("/analyze")
-def analyze(data: dict):
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-try:
-    from resume_parser import extract_text_from_pdf
-    from skill_extractor import extract_skills_with_llm
-    from scoring import calculate_skill_score, semantic_match_score
-except Exception as e:
-    print("IMPORT ERROR:", e)
-
 app = FastAPI()
 
 
+# Request schema
 class RequestData(BaseModel):
     resume_text: str
     job_description: str
 
 
+# Root endpoint
 @app.get("/")
 def home():
     return {"message": "API is running"}
 
 
+# Main API endpoint
 @app.post("/analyze")
 def analyze(data: RequestData):
 
-    resume_skills = extract_skills_with_llm(data.resume_text)
-    jd_skills = extract_skills_with_llm(data.job_description)
+    try:
+        # Lazy imports
+        from resume_parser import extract_text_from_pdf
+        from skill_extractor import extract_skills_with_llm
+        from scoring import calculate_skill_score, semantic_match_score
 
-    skill_score, matched, missing = calculate_skill_score(
-        resume_skills,
-        jd_skills
-    )
+        resume_skills = extract_skills_with_llm(data.resume_text)
+        jd_skills = extract_skills_with_llm(data.job_description)
 
-    semantic_score = semantic_match_score(
-        data.resume_text,
-        data.job_description
-    )
+        skill_score, matched, missing = calculate_skill_score(
+            resume_skills,
+            jd_skills
+        )
 
-    final_score = int((skill_score * 0.3) + (semantic_score * 0.7))
+        semantic_score = semantic_match_score(
+            data.resume_text,
+            data.job_description
+        )
 
-    return {
-        "final_score": final_score,
-        "skill_score": skill_score,
-        "semantic_score": semantic_score,
-        "matched": matched,
-        "missing": missing
-    }
+        final_score = int((skill_score * 0.3) + (semantic_score * 0.7))
+
+        return {
+            "final_score": final_score,
+            "skill_score": skill_score,
+            "semantic_score": semantic_score,
+            "matched": matched,
+            "missing": missing
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
